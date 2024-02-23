507308b559da3059d8f4c4f0a0146add0089635e
xxxbobrxxx <xxxbobrxxx@gmail.com>
2024-02-23 19:30:33 +0100

    Add parallel repository download support

--- cmd/command-generate.go
+++ cmd/command-generate.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 	"github.com/xxxbobrxxx/ide-gen/pkg/config"
 	"github.com/xxxbobrxxx/ide-gen/pkg/idea"
 	"github.com/xxxbobrxxx/ide-gen/pkg/repository"
+	"runtime"
+	"sync"
 )
 
 type GenerateCommand struct {
 	config.GlobalFlags
 	repository.SourcesRootFlags
 	idea.Project
+
+	Parallel            bool
+	ParallelConcurrency int
 
 	cmd *cobra.Command
 }
@@ -31,6 +37,7 @@ func NewGenerateCommand() *GenerateCommand {
 	command.Project.AddFlags(cmd.PersistentFlags())
 	command.GlobalFlags.AddFlags(cmd.PersistentFlags())
 	command.SourcesRootFlags.AddFlags(cmd.PersistentFlags())
+	command.AddFlags(cmd.PersistentFlags())
 
 	_ = command.cmd.MarkPersistentFlagRequired("config")
 
@@ -39,6 +46,13 @@ func NewGenerateCommand() *GenerateCommand {
 
 func (command *GenerateCommand) Register() *cobra.Command {
 	return command.cmd
+}
+
+func (command *GenerateCommand) AddFlags(flags *pflag.FlagSet) {
+	flags.BoolVarP(&command.Parallel, "parallel", "p",
+		false, "Use parallel download")
+	flags.IntVar(&command.ParallelConcurrency, "parallel-concurrency", runtime.NumCPU()*2,
+		"Parallel download concurrency, default is runtime.NumCPU() * 2")
 }
 
 func (command *GenerateCommand) ProcessProjectEntry(projectEntry repository.ProjectEntry) (err error) {
@@ -71,9 +85,29 @@ func (command *GenerateCommand) Execute(_ *cobra.Command, _ []string) (err error
 	}
 
 	//: Clone repos
-	for _, projectEntry := range projectEntries {
-		if err := command.ProcessProjectEntry(projectEntry); err != nil {
-			return err
+	if command.Parallel {
+		sem := make(chan struct{}, command.ParallelConcurrency)
+		var wg sync.WaitGroup
+
+		for _, projectEntry := range projectEntries {
+			wg.Add(1)
+			sem <- struct{}{}
+			go func(projectEntry repository.ProjectEntry) {
+				defer wg.Done()
+				defer func() { <-sem }()
+
+				err := command.ProcessProjectEntry(projectEntry)
+				if err != nil {
+					logger.WithError(err).Errorf("error cloning the repo: %v", projectEntry.Name)
+				}
+			}(projectEntry)
+		}
+		wg.Wait()
+	} else {
+		for _, projectEntry := range projectEntries {
+			if err := command.ProcessProjectEntry(projectEntry); err != nil {
+				logger.WithError(err).Errorf("error cloning the repo: %v", projectEntry.Name)
+			}
 		}
 	}
 
