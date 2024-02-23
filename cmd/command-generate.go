f7ff3b64779480c01e36740fb65d5842078198aa
Dmytro Hopkalo <d.hopkalo@criteo.com>
2024-02-23 18:05:57 +0100

    - Add goroutine usage for generate command with limited worker's pull

--- cmd/command-generate.go
+++ cmd/command-generate.go
@@ -5,6 +5,8 @@ import (
 	"github.com/xxxbobrxxx/ide-gen/pkg/config"
 	"github.com/xxxbobrxxx/ide-gen/pkg/idea"
 	"github.com/xxxbobrxxx/ide-gen/pkg/repository"
+	"runtime"
+	"sync"
 )
 
 type GenerateCommand struct {
@@ -47,33 +49,42 @@ func (command *GenerateCommand) Execute(_ *cobra.Command, _ []string) (err error
 		return err
 	}
 
-	//: Read entries from config and flags
 	projectEntries, err := c.GetProjectEntries(command.SourcesRootFlags)
 	if err != nil {
 		return err
 	}
 
-	//: Clone repos
+	// Determine the number of workers based on CPU count
+	numWorkers := runtime.NumCPU() * 2
+	// Create a buffered channel to limit concurrent goroutines
+	sem := make(chan struct{}, numWorkers)
+
+	var wg sync.WaitGroup
 	for _, projectEntry := range projectEntries {
-		exists, err := projectEntry.Commander.Exists(projectEntry.Directory)
-		if err != nil {
-			return err
-		}
+		wg.Add(1)
+		sem <- struct{}{} // Acquire a slot
+		go func(entry repository.ProjectEntry) {
+			defer wg.Done()
+			defer func() { <-sem }() // Release the slot
 
-		if exists {
-			logger.Infof(
-				"Skip clone project '%s' to '%s'", projectEntry.Name, projectEntry.Directory)
-		} else {
-			logger.Infof(
-				"Clone project '%s' to '%s'", projectEntry.Name, projectEntry.Directory)
-			err = projectEntry.Commander.Clone(projectEntry.Directory)
+			exists, err := entry.Commander.Exists(entry.Directory)
 			if err != nil {
-				return err
+				logger.Errorf("Error checking existence: %v", err)
+				return
 			}
-		}
+			if exists {
+				logger.Infof("Skip clone project '%s' to '%s'", entry.Name, entry.Directory)
+			} else {
+				logger.Infof("Clone project '%s' to '%s'", entry.Name, entry.Directory)
+				if err := entry.Commander.Clone(entry.Directory); err != nil {
+					logger.Errorf("Error cloning project: %v", err)
+				}
+			}
+		}(projectEntry)
 	}
+	wg.Wait() // Wait for all goroutines to finish
 
-	//: Idea project
+	// Idea project
 	if command.Project.Root != "" {
 		project := command.Project
 		for _, projectEntry := range projectEntries {
@@ -81,8 +92,7 @@ func (command *GenerateCommand) Execute(_ *cobra.Command, _ []string) (err error
 		}
 
 		logger.Infof("Writing idea project %s", project.Root)
-		err = project.Write()
-		if err != nil {
+		if err := project.Write(); err != nil {
 			return err
 		}
 	}
